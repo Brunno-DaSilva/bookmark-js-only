3536d71c35744d60b04dc7fcafa59cb7753d0416
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-12-02 15:44:04 -0600

    Added form, btn, and input style

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 console.log("Hello World");
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM fully loaded and parsed");
+  const form = document.getElementById("bookmark__form");
+});
