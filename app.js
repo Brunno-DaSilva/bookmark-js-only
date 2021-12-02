92cf8c2ea675dee95ddae2594009c8e94ea00dc1
Bruno DaSilva <hi.dasilvab@gmail.com>
2021-12-02 16:09:37 -0600

    Added JS script with local storage, tested items are being stored

--- app.js
+++ app.js
@@ -3,4 +3,29 @@ console.log("Hello World");
 document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM fully loaded and parsed");
   const form = document.getElementById("bookmark__form");
+
+  function saveBookmark(e) {
+    e.preventDefault();
+    const siteName = document.getElementById("siteName").value;
+    const siteUrl = document.getElementById("siteUrl").value;
+
+    const bookmark = {
+      name: siteName,
+      url: siteUrl,
+    };
+
+    console.log(bookmark);
+
+    if (localStorage.getItem("bookmarks") === null) {
+      const bookmarks = [];
+      bookmarks.push(bookmark);
+      localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+    } else {
+      const bookmarks = JSON.parse(localStorage.getItem("bookmarks"));
+      bookmarks.push(bookmark);
+      localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
+    }
+  }
+
+  form.addEventListener("submit", saveBookmark);
 });
